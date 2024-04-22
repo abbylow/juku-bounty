bb79b842d6815505df9ce6a83adc818048855cf4
Abby L <yu950713@gmail.com>
2024-04-22 17:56:42 +0800

    feat: change bell icon with red dot; hide nav items in mobile view

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -17,14 +17,13 @@ import {
 } from "@/components/ui/sheet"
 
 // TODO: mobile responsiveness
-// TODO: logged in view - profile, notification, verified badge, platform point
 // TODO: TBD create quest -> hover and expand nav menu for bounty and consultation request creation
 export function Header() {
   const pathname = usePathname()
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-20 max-w-screen-2xl items-center justify-between gap-4">
-        <div className="flex items-center justify-start gap-8">
+        <div className="flex items-center justify-start gap-6">
           {/* TODO: switch logo back to next/link */}
           {/* <Link href="/" className="flex items-center space-x-2">
             <Icons.logo className="w-20 bg-white px-2" />
@@ -36,7 +35,7 @@ export function Header() {
           <Link
             href={APP_HOMEPAGE_URL}
             className={cn(
-              "transition-colors hover:text-foreground/80",
+              "transition-colors hover:text-foreground/80 md:block hidden",
               pathname?.startsWith(APP_HOMEPAGE_URL)
                 ? "text-foreground"
                 : "text-foreground/60"
@@ -47,7 +46,7 @@ export function Header() {
           <Link
             href={BOUNTY_CREATION_URL}
             className={cn(
-              "transition-colors hover:text-foreground/80",
+              "transition-colors hover:text-foreground/80 md:block hidden",
               pathname?.startsWith(BOUNTY_CREATION_URL)
                 ? "text-foreground"
                 : "text-foreground/60"
@@ -58,7 +57,7 @@ export function Header() {
           <Link
             href={CONSULTATION_CREATION_URL}
             className={cn(
-              "transition-colors hover:text-foreground/80",
+              "transition-colors hover:text-foreground/80 md:block hidden",
               pathname?.startsWith(CONSULTATION_CREATION_URL)
                 ? "text-foreground"
                 : "text-foreground/60"
@@ -66,7 +65,7 @@ export function Header() {
           >
             Request Consultation
           </Link>
-          <a href={FAQ_URL} rel="noreferrer noopener" target="_blank" className="transition-colors text-foreground/60 hover:text-foreground/80">
+          <a href={FAQ_URL} rel="noreferrer noopener" target="_blank" className="transition-colors text-foreground/60 hover:text-foreground/80 md:block hidden">
             FAQ
           </a>
 
@@ -74,20 +73,20 @@ export function Header() {
         <div className="flex items-center justify-end gap-4">
           {/* TODO: start here - hide these buttons if user is not logged in */}
           {/* TODO: TBD - where to link this route to? */}
-          <Link href={PROFILE_URL}>
+          <Link href={PROFILE_URL} className="md:block hidden">
             <Award className="h-6 w-6" />
           </Link>
 
           {/* TODO: TBD - where to link this route to? */}
-          <Link href={PROFILE_URL}>
+          <Link href={PROFILE_URL} className="md:block hidden">
             <ShieldCheck className="h-6 w-6" />
           </Link>
 
           <Sheet>
-            <SheetTrigger>
+            <SheetTrigger className="md:block hidden">
               {/* TODO: determine if there is notification, show BellDot, else show Bell */}
               {/* <Bell className="h-6 w-6" /> */}
-              <BellDot className="h-6 w-6" />
+              <Icons.bellDot className="h-6 w-6" />
             </SheetTrigger>
             <SheetContent>
               <SheetHeader>
