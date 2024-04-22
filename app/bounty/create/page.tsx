103bc051e2d8b7283c34d79ada3906216412791d
Abby L <yu950713@gmail.com>
2024-04-22 17:28:41 +0800

    chore: add TODO comments

--- app/bounty/create/page.tsx
+++ app/bounty/create/page.tsx
@@ -1,3 +1,4 @@
+// TODO: if user is not logged, prompt user to login
 export default function BountyCreation() {
   return (
     <main className="container min-h-screen flex flex-col items-center">
