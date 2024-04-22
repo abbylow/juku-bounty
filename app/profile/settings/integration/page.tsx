784709f899ecbd4142d50a0ea090d92fe4c1cfdf
Abby L <yu950713@gmail.com>
2024-04-23 04:10:32 +0800

    feat: add profile settings integration form

--- app/profile/settings/integration/page.tsx
+++ app/profile/settings/integration/page.tsx
@@ -1,17 +1,19 @@
 import { Separator } from "@/components/ui/separator";
+import { ProfileIntegrationForm } from "@/app/profile/settings/integration/form";
 
 export default function ProfileSettingsIntegration() {
   return (
     <section>
       <div className="space-y-6">
         <div className="space-y-2">
-        <h3 className="text-lg font-medium">{"My Settings > Integration"}</h3>
+          <h3 className="text-lg font-medium">{"My Settings > Integration"}</h3>
           <p className="text-sm text-muted-foreground">
+            {/* TODO: this copy here mismatches the purpose of integration */}
             Connect to your wallet to start creating quests or receive rewards.
           </p>
         </div>
         <Separator />
-        <div>integration form here</div>
+        <ProfileIntegrationForm />
       </div>
     </section>
   )
