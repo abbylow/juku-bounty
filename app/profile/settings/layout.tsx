181f4a84f1e208db2463c0d080cb8f8e7599f996
Abby L <yu950713@gmail.com>
2024-05-19 21:21:06 +0800

    chore: add todo

--- app/profile/settings/layout.tsx
+++ app/profile/settings/layout.tsx
@@ -5,6 +5,7 @@ import { SidebarNav } from "@/components/ui/sidebar-nav"
 import { PROFILE_SETTINGS_INTEGRATION_URL, PROFILE_SETTINGS_NOTIFICATION_URL, PROFILE_SETTINGS_PRIVACY_URL, PROFILE_SETTINGS_URL } from "@/const/links"
 import { ConnectBtn } from "@/components/thirdweb/connect-btn";
 
+// TODO: mobile responsiveness - tabs
 const sidebarNavItems = [
   {
     title: "Profile",
