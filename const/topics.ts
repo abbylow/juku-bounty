913f25d173ef88aaeeebc571b0f7dc8d8425e52c
Abby L <yu950713@gmail.com>
2024-07-31 23:19:29 +0800

    feat: add constants for bounty templates and default topics

---
+++ const/topics.ts
@@ -0,0 +1,18 @@
+// Bounty topics
+import { Option } from '@/components/ui/multiple-selector';
+
+// TODO: show some default hardcoded tags 
+// TODO: get from ceramic 
+export const TOPIC_OPTIONS: Option[] = [
+  { label: 'Nextjs', value: 'nextjs' },
+  { label: 'React', value: 'react' },
+  { label: 'Remix', value: 'remix' },
+  { label: 'Vite', value: 'vite' },
+  { label: 'Nuxt', value: 'nuxt' },
+  { label: 'Vue', value: 'vue' },
+  { label: 'Svelte', value: 'svelte' },
+  { label: 'Angular', value: 'angular' },
+  { label: 'Ember', value: 'ember' },
+  { label: 'Gatsby', value: 'gatsby' },
+  { label: 'Astro', value: 'astro' },
+];
