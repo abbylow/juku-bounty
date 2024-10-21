63882062355ecac275d450048db5520017c1cc1e
Abby L <yu950713@gmail.com>
2024-10-21 22:35:14 +0800

    backup: add sql to create default categories

---
+++ sql/seed_categories.sql
@@ -0,0 +1,19 @@
+INSERT INTO Category (name, slug, created_at, edited_at)
+VALUES 
+    ('Entrepreneurship', 'entrepreneurship', NOW(), NOW()),
+    ('Business Development & Strategy', 'business_development_and_strategy', NOW(), NOW()),
+    ('Product', 'product', NOW(), NOW()),
+    ('Art & Design', 'art_and_design', NOW(), NOW()),
+    ('Web3 & Crypto', 'web3_and_crypto', NOW(), NOW()),
+    ('Software Engineering & Cybersecurity', 'software_engineering_and_cybersecurity', NOW(), NOW()),
+    ('Marketing', 'marketing', NOW(), NOW()),
+    ('Social Media & Community', 'social_media_and_community', NOW(), NOW()),
+    ('Data & Artificial Intelligence', 'data_and_ai', NOW(), NOW()),
+    ('Talent', 'talent', NOW(), NOW()),
+    ('Finance & Investment', 'finance_and_investment', NOW(), NOW()),
+    ('Legal & Regulation', 'legal_and_regulation', NOW(), NOW()),
+    ('Health, Fitness & Sports', 'health_fitness_and_sports', NOW(), NOW()),
+    ('Gaming', 'gaming', NOW(), NOW()),
+    ('Music & Entertainment', 'music_and_entertainment', NOW(), NOW()),
+    ('Education', 'education', NOW(), NOW()),
+    ('Others', 'others', NOW(), NOW());
