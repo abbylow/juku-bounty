5572215ef155e4d0a5ed9acc6648b7a78a220e05
Abby L <yu950713@gmail.com>
2024-11-24 18:27:23 +0800

    chore: refactor - extract contribution form to another file

--- components/bounty/index.tsx
+++ components/bounty/index.tsx
@@ -3,38 +3,29 @@
 import { useQuery } from "@tanstack/react-query"
 import { formatDistance, formatDistanceToNow } from 'date-fns'
 import { formatUnits } from "ethers/lib/utils"
-import { Award, CalendarClock, Lightbulb, ChevronRight, Info } from "lucide-react"
+import { Award, CalendarClock, ChevronRight } from "lucide-react"
 import Link from "next/link"
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import { useCallback, useEffect, useState } from "react"
 import { getContract } from "thirdweb"
 import { decimals } from "thirdweb/extensions/erc20"
 import { useActiveAccount, useReadContract } from "thirdweb/react"
 
-import { createContribution } from "@/actions/contribution/createContribution"
 import { getProfile } from "@/actions/profile/getProfile"
-import { getProfiles } from "@/actions/profile/getProfiles"
 import { Tag } from "@/actions/tag/type"
 import BountyLikeButton from "@/components/bounty/like-button"
 import BountyShareButton from "@/components/bounty/share-button"
+import ContributionForm from "@/components/contribution/form"
 import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
   CardHeader,
   CardFooter,
 } from "@/components/ui/card"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
-import { Label } from "@/components/ui/label"
-import MultipleSelector, { Option } from '@/components/ui/multiple-selector'
 import { Skeleton } from "@/components/ui/skeleton"
 import { Separator } from "@/components/ui/separator"
-import { Textarea } from "@/components/ui/textarea"
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
-import { toast } from "@/components/ui/use-toast"
 import UserAvatar from "@/components/user/avatar"
-import { useViewerContext } from "@/contexts/viewer"
 import { BountyStatus } from "@/const/bounty-status"
 import { currentChain } from "@/const/chains"
 import { tokenAddressToTokenNameMapping } from "@/const/contracts"
@@ -45,9 +36,7 @@ import { client } from "@/lib/thirdweb-client"
 
 export default function BountyCard({ details }: { details: any }) {
   const pathname = usePathname();
-  const router = useRouter();
   const activeAccount = useActiveAccount();
-  const { viewer } = useViewerContext();
 
   // Get bounty's reward details from escrow contract
   const { data: bountyData, isPending: isBountyDataPending } = useReadContract({
@@ -118,59 +107,6 @@ export default function BountyCard({ details }: { details: any }) {
     return profile
   };
 
-  const [dialogOpen, setDialogOpen] = useState(false);
-
-  const [contributionDescDisabled, setContributionDescDisabled] = useState(false);
-
-  const [contributionDesc, setContributionDesc] = useState("");
-  const handleDescChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setContributionDesc(e.target.value)
-  }
-
-  const [referee, setReferee] = useState<Option[]>();
-
-  useEffect(() => {
-    if (referee?.length) {
-      setContributionDescDisabled(true);
-      setContributionDesc(`I refer ${referee[0].label} to contribute to this quest`);
-    } else {
-      setContributionDescDisabled(false);
-    }
-  }, [referee])
-
-
-  const searchUsers = async (value: string): Promise<Option[]> => {
-    const response = await getProfiles({
-      username: value,
-      display_name: value,
-    });
-    // TODO: remove viewer itself
-    // TODO: for users that have disabled referral, user will not be able to choose and refer them and see the “Referral disabled” tag
-    const options = response.map((profile) => ({
-      value: profile.id,
-      label: `${profile.display_name} @${profile.username}`, // Combine display name and username for the label
-    }));
-    return options;
-  };
-
-  const handleSubmission = async () => {
-    try {
-      await createContribution({
-        bountyId: details.id,
-        creatorProfileId: viewer?.id!,
-        description: contributionDesc,
-        referreeId: referee ? referee[0].value : ''
-      })
-      setDialogOpen(false);
-      setContributionDesc("");
-      setReferee([]);
-      toast({ title: "Successfully created contribution." });
-      router.push(`/bounty/${details.id}`)
-    } catch (error) {
-      console.log("Something went wrong")
-    }
-  }
-
   if (isCreatorProfilePending || isBountyDataPending) {
     return <Skeleton className="h-56" />
   }
@@ -236,76 +172,7 @@ export default function BountyCard({ details }: { details: any }) {
             <BountyLikeButton bountyId={details.id} />
           </div>
           {/* TODO: handle dynamic CTA - contribute / end quest / edit quest?  */}
-          <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-            <DialogTrigger asChild>
-              <Button>
-                <Lightbulb className="mr-2 h-5 w-5" /> Contribute
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:w-5/6">
-              <DialogHeader>
-                <DialogTitle>Contribute to Bounty</DialogTitle>
-                <DialogDescription>
-                  By submitting your response or refer a connection to contribute, a publicly visible thread will be opened among quester, you (and referrer) for any follow-up discussion.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid gap-2">
-                  <Label htmlFor="response" className="">
-                    Enter your response or provide any relevant links
-                  </Label>
-                  <Textarea
-                    id="response"
-                    className="resize-y h-40"
-                    value={contributionDesc}
-                    onChange={handleDescChange}
-                    disabled={contributionDescDisabled}
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="referree" className="flex gap-1">
-                    Refer user to contribute (optional)
-                    <TooltipProvider>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <Info className="h-4 w-4 text-muted-foreground" />
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>You will earn 20% of the rewards if your referral wins the bounty rewards</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </TooltipProvider>
-                  </Label>
-                  <MultipleSelector
-                    value={referee}
-                    onChange={setReferee}
-                    maxSelected={1}
-                    onMaxSelected={(maxLimit) => {
-                      toast({ title: `You have reached max selected referee: ${maxLimit}` });
-                    }}
-                    onSearch={async (value) => {
-                      return await searchUsers(value);
-                    }}
-                    placeholder="Search by display name or username"
-                    loadingIndicator={
-                      <p className="py-2 text-center text-lg leading-10 text-muted-foreground">loading...</p>
-                    }
-                    emptyIndicator={
-                      <p className="w-full text-center text-lg leading-10 text-muted-foreground">
-                        no user found.
-                      </p>
-                    }
-                    hideClearAllButton
-                  />
-                </div>
-              </div>
-              <DialogFooter>
-                <div className="flex flex-col gap-4">
-                  <Button type="submit" onClick={handleSubmission}>Submit</Button>
-                </div>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+          <ContributionForm bountyId={details.id} />
         </div>
       </CardFooter>}
     </Card>
